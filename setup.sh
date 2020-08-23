01b749f415dd044225dd9ea7e6556d972f09ef20
Kelvin Petry <test@test.de>
2020-08-23 21:34:57 +0200

    switching to selfcontained git repo

--- setup.sh
+++ setup.sh
@@ -2,10 +2,9 @@
 
 echo $1 | sudo -S localectl set-keymap de
 
-sudo pacman-mirrors --fasttrack 5 && sudo pacman -Syyu --noconfirm
 sudo pacman -R --noconfirm pcmanfm
 sudo pacman -Rd --nodeps --noconfirm picom 
-sudo pacman -S --noconfirm asciidoc uthash gcc meson virtualbox-guest-utils yay xclip feh firefox ranger rofi docker polybar zsh zsh-completions tilix flameshot
+sudo pacman -S --noconfirm asciidoc uthash gcc meson virtualbox-guest-utils xclip feh firefox ranger rofi docker polybar zsh zsh-completions tilix flameshot
 
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/.oh-my-zsh/custom/themes/powerlevel10k
@@ -19,13 +18,14 @@ echo 'inode/directory=ranger.desktop' >> ../.config/mimeapps.list
 sudo tar -C /usr/share/themes -xvf other/Orchis-light.tar.xz
 sudo usermod --shell $(which zsh) $USER
 chsh -s $(which zsh) <<< $1
-dconf load /com/gexperts/Tilix/ < ./other/tilix.dconf
+dconf load /com/gexperts/Tilix/ < ./tilix.dconf
 
 mkdir ../.config/polybar
-mv ./other/launch.sh ../.config/polybar/launch.sh
-sudo chmod +x ../.config/polybar/launch.sh
 
 #docker
 echo $1 | sudo -S systemctl start docker.service
 sudo systemctl enable docker.service
 sudo usermod -aG docker $USER
+
+#finally override again
+yadm clone $2
