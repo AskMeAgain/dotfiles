0c731606549d45da28c1b4eac404e15fa151fcda
Kelvin Petry <test@test.de>
2020-10-03 20:39:40 +0200

    adding small improvements to rofi startup

--- .i3/open_in_new_workspace.sh
+++ .i3/open_in_new_workspace.sh
@@ -1,4 +1,9 @@
 #!/bin/zsh
 
 WS=$(($(i3-msg -t get_workspaces | tr , '\n' | grep '"num":' | cut -d : -f 2 | sort -rn | head -1) + 1))
-i3-msg "workspace number $WS; exec --no-startup-id tilix -a app-new-window -x 'zsh -c $1'"
+
+
+if [ "$2" = "--in-console" ]; 
+then i3-msg "workspace number $WS; exec --no-startup-id tilix -a app-new-window -x 'zsh -c ranger'"
+else i3-msg "workspace number $WS; exec --no-startup-id $1";
+fi
