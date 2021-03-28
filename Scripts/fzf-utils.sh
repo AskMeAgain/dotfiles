47da1909198fb51840b123778ab2091f5c694351
Kelvin Petry <test@test.de>
2021-03-28 09:40:46 +0200

    adding fzf

--- Scripts/fzf-utils.sh
+++ Scripts/fzf-utils.sh
@@ -1,25 +1,29 @@
 #!/bin/zsh
 
+
 source /usr/share/fzf/key-bindings.zsh
 source /usr/share/fzf/completion.zsh
 
+export FZF_DEFAULT_OPTS='--height="40%" --info=inline --reverse'
+
+alias bookmarks='eval $(alias + | fzf)'
+
 fe() {
-  IFS=$'\n' files=($(fzf-tmux --query="$1" --height='40%' --reverse --info=inline --preview 'cat {}' --multi --select-1 --exit-0))
+  IFS=$'\n' files=($(fzf-tmux --query="$1" --preview 'cat {}' --multi --select-1 --exit-0))
   [[ -n "$files" ]] && ${EDITOR:-vim} "${files[@]}"
 }
 
-# fh - repeat history
 fh() {
-  eval $( ([ -n "$ZSH_NAME" ] && fc -l 1 || history) | fzf --height='40%' --reverse --info=inline +s --tac | sed -E 's/ *[0-9]*\*? *//' | sed -E 's/\\/\\\\/g')
+  eval $( ([ -n "$ZSH_NAME" ] && fc -l 1 || history) | fzf | sed -E 's/ *[0-9]*\*? *//' | sed -E 's/\\/\\\\/g')
 }
 
 dl() {
   local cid
-  cid=$(docker ps -a --format "table {{.ID}}_\t{{.Image}}\t{{.Status}}\t{{.Ports}}" \
+  cid=$(docker ps -a --format "table {{.ID}} \t{{.Image}}\t{{.Status}}\t{{.Ports}}" \
   | sed 1d | fzf --height='60%' \
-  --preview 'docker logs $(echo {} | cut -d "_" -f1)' \
-  --preview-window follow:12:down:rounded:wrap:cycle \
-  --reverse --info=inline -1 -q "$1" \
+  --preview 'docker logs $(echo {} | cut -d " " -f1)' \
+  --preview-window follow:12:down:rounded:wrap \
+  -1 -q "$1" \
   | awk '{print $1}')
 
   [ -n "$cid" ] && docker logs "$cid" -f
@@ -27,7 +31,8 @@ dl() {
 
 drm() {
   local cid
-  cid=$(docker ps --format "table {{.ID}}\t{{.Image}}\t{{.Status}}\t{{.Ports}}" | sed 1d | fzf --height='40%' --reverse --info=inline --multi -1 -q "$1" | awk '{print $1}')
+  cid=$(docker ps --format "table {{.ID}}\t{{.Image}}\t{{.Status}}\t{{.Ports}}" | sed 1d \
+  | fzf --multi -1 -q "$1" | awk '{print $1}')
 
 	if [[ $cid ]]; then
       for dockerId in $(echo $cid);
