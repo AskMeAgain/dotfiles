1b8bd538fc48d7a3327fe17db90196a59acb6bfc
Kelvin Petry <test@test.de>
2021-03-28 15:47:02 +0200

    adding fzf

--- Scripts/aliases.sh
+++ Scripts/aliases.sh
@@ -1,12 +1,11 @@
 #!/bin/sh
 
-## Alias section 
 alias ccc="xclip -sel clip"
 alias ls='ls --color=auto'
 alias grep='grep --color=auto'
-alias nano='micro'
+alias shutdown='shutdown now'
 alias dps='docker ps -a --format "table {{.ID}}\t{{.Image}}\t{{.Status}}\t{{.Ports}}"'
+
 if [ $UID -ne 0 ]; then
     alias reboot="echo '1234' | sudo -S reboot"
 fi
-alias shutdown='shutdown now'
