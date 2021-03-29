7fcce94b897a0848ed3012b33501c0fbc46fc651
Kelvin Petry <test@test.de>
2021-03-29 19:48:47 +0200

    Adding Konsole

--- Scripts/change_style.sh
+++ Scripts/change_style.sh
@@ -10,6 +10,8 @@ else
     writeGtkTheme --dark
 fi
 
+fixKonsoleTheme
+
 dconf load /com/gexperts/Tilix/ < /home/dev/.cache/wal/tilix.dconf
 mv /home/dev/.cache/wal/ranger-style.py /home/dev/.config/ranger/colorschemes/orchid.py
 mv /home/dev/.cache/wal/flameshot.ini /home/dev/.config/flameshot/flameshot.ini
--- Scripts/utils.sh
+++ Scripts/utils.sh
@@ -25,6 +25,10 @@ sed -i "s/gtk-theme-name=.*/gtk-theme-name=$TMP/g" /home/dev/.config/gtk-3.0/set
 
 }
 
+fixKonsoleTheme(){
+	sed -i "s/Opacity=/Opacity=0./g" /home/dev/.cache/wal/colors-konsole.colorscheme
+}
+
 not_in_console=("firefox" "google-chrome-stable" "idea" "idea.sh" "postman" "dbeaver") 
 
 openProgram(){
