bf6c2712cf7e1dbe95c3778409c784a1cf20feb1
Kelvin Petry <test@test.de>
2021-09-08 22:13:35 +0200

    switching to manjaro 21.1

--- Scripts/change_style.sh
+++ Scripts/change_style.sh
@@ -13,4 +13,3 @@ fi
 dconf load /com/gexperts/Tilix/ < /home/dev/.cache/wal/tilix.dconf
 mv /home/dev/.cache/wal/ranger-style.py /home/dev/.config/ranger/colorschemes/orchid.py
 mv /home/dev/.cache/wal/flameshot.ini /home/dev/.config/flameshot/flameshot.ini
-mv /home/dev/.cache/wal/KonsoleTheme.colorscheme /home/dev/.local/share/konsole/KonsoleTheme.colorscheme
