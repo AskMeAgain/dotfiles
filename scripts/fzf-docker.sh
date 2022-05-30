eac14cb7f62e07e1b819f12ac31efdbd25b82bff
Kelvin Petry <test@test.de>
2022-05-30 20:58:07 +0200

    fixing some stuff

--- scripts/fzf-docker.sh
+++ scripts/fzf-docker.sh
@@ -1,91 +1,45 @@
-#!/bin/zsh
+#!/bin/sh
 
-source /usr/share/fzf/key-bindings.zsh
-source /usr/share/fzf/completion.zsh
+fzf_docker(){
+	all_flag=$1
+	shift
+	cid=$(docker ps "$all_flag" --format "table {{.ID}}\t{{.Image}}\t{{.Status}}\t{{.Ports}}" | sed 1d \
+	  | fzf --preview 'docker logs $(echo {} | cut -d " " -f1) -n $FZF_PREVIEW_LINES' --height='70%' \
+	  --header="Select Container to go into" --preview-window follow:50%:down:rounded:wrap --multi -1 -q "$1" | awk '{print $1}')
 
-docker_logs() {
-  local cid
-  cid=$(docker ps -a --format "table {{.ID}} \t{{.Image}}\t{{.Status}}\t{{.Ports}}" \
-  | sed 1d | fzf --height='70%' --header="Select Container to be removed" \
-  --preview 'docker logs $(echo {} | cut -d " " -f1) -n $FZF_PREVIEW_LINES'  --header="Select Container to display logs" \
-  --preview-window follow:50%:down:rounded:wrap -1 -q "$1" \
-  | awk '{print $1}')
-
-  [ -n "$cid" ] && docker logs "$cid" -f
+	[ -n "$cid" ] && echo "$cid"
 }
 
-docker_rm() {
-  local cid
-  cid=$(docker ps -a --format "table {{.ID}} \t{{.Image}}\t{{.Status}}\t{{.Ports}}" | sed 1d \
-  | fzf --preview 'docker logs $(echo {} | cut -d " " -f1) -n $FZF_PREVIEW_LINES' --height='70%'  --header="Select Containers to be removed" \
-  --preview-window follow:50%:down:rounded:wrap --print0 --multi -1 -q "$1" | awk '{print $1}')
-  
-  docker rm -f $cid
+dex(){
+	cid=$(fzf_docker "-s" $@)
+	docker exec -it "$cid" sh
 }
 
-docker_restart() {
-  local cid
-  cid=$(docker ps -a --format "table {{.ID}} \t{{.Image}}\t{{.Status}}\t{{.Ports}}" | sed 1d \
-  | fzf --preview 'docker logs $(echo {} | cut -d " " -f1) -n $FZF_PREVIEW_LINES' --height='70%'  --header="Select Containers to be removed" \
-  --preview-window follow:50%:down:rounded:wrap --print0 --multi -1 -q "$1" | awk '{print $1}')
-  
-  docker restart $cid
+dl(){
+	fzf_docker "-a" $@ | xargs -I{} docker logs {} -f	
 }
 
-docker_stop() {
-  local cid
-  cid=$(docker ps -a --format "table {{.ID}} \t{{.Image}}\t{{.Status}}\t{{.Ports}}" | sed 1d \
-  | fzf --preview 'docker logs $(echo {} | cut -d " " -f1) -n $FZF_PREVIEW_LINES' --height='70%'  --header="Select Containers to be removed" \
-  --preview-window follow:50%:down:rounded:wrap --print0 --multi -1 -q "$1" | awk '{print $1}')
-  
-  docker stop $cid
+dsta(){
+	fzf_docker "-a" $@ | xargs -I{} docker start {}
 }
 
-docker_start() {
-  local cid
-  cid=$(docker ps -a --format "table {{.ID}} \t{{.Image}}\t{{.Status}}\t{{.Ports}}" | sed 1d \
-  | fzf --preview 'docker logs $(echo {} | cut -d " " -f1) -n $FZF_PREVIEW_LINES' --height='70%'  --header="Select Containers to be removed" \
-  --preview-window follow:50%:down:rounded:wrap --print0 --multi -1 -q "$1" | awk '{print $1}')
-  
-  docker start $cid
+dsto(){
+	fzf_docker "-s" $@ | xargs -I{} docker stop {}
 }
 
-docker_restart() {
-  local cid
-  cid=$(docker ps -a --format "table {{.ID}} \t{{.Image}}\t{{.Status}}\t{{.Ports}}" | sed 1d \
-  | fzf --preview 'docker logs $(echo {} | cut -d " " -f1) -n $FZF_PREVIEW_LINES' --height='70%'  --header="Select Containers to be removed" \
-  --preview-window follow:50%:down:rounded:wrap --print0 --multi -1 -q "$1" | awk '{print $1}')
-  
-  docker restart $cid
+di(){
+	fzf_docker "-a" $@ | xargs -I{} docker inspect {}
 }
 
-docker_exec() {
-  local cid
-  cid=$(docker ps -a --format "table {{.ID}}\t{{.Image}}\t{{.Status}}\t{{.Ports}}" | sed 1d \
-  | fzf --preview 'docker logs $(echo {} | cut -d " " -f1) -n $FZF_PREVIEW_LINES' --height='70%' \
-     --header="Select Container go into" --preview-window follow:50%:down:rounded:wrap --multi -1 -q "$1" | awk '{print $1}')
-
-  STATE=$(docker inspect $cid | jq '.[0].State.Status')
-  
-  if [[ $STATE == *"unnin"* ]];
-  then
-    [ -n "$cid" ] && \docker exec -it "$cid" bash
-  else
-	docker commit $cid temp/tempcontainer
-	docker run -it temp/tempcontainer sh
-  fi
+dre(){
+	fzf_docker "-s" $@ | xargs -I{} docker restart {}
 }
 
-docker_inspect() {
-  local cid
-  cid=$(docker ps -a --format "table {{.ID}}\t{{.Image}}\t{{.Status}}\t{{.Ports}}" | sed 1d \
-  | fzf --preview 'docker logs $(echo {} | cut -d " " -f1) -n $FZF_PREVIEW_LINES' --height='70%' \
-  --header="Select Container go into" --preview-window follow:50%:down:rounded:wrap --multi -1 -q "$1" | awk '{print $1}')
-
-  [ -n "$cid" ] && docker inspect "$cid"
+drm(){
+	fzf_docker "-a" $@ | xargs -I{} docker rm -f {}
 }
 
-docker_ps() {
+dps() {
   if [[ -z "$1" ]];
   then
     docker ps -a --format "table {{.ID}}\t{{.Image}}\t{{.Status}}\t{{.Ports}}"
@@ -95,32 +49,4 @@ docker_ps() {
       sleep $1
     done
   fi
-}
-
-d() {
-    CMD=$1
-    shift
-    if [ -z "$CMD" ]; then
-        docker
-    else
-        if [ "$CMD" = "inspect" ]; then
-          docker_inspect $@
-        elif [ "$CMD" = "rm" ]; then
-          docker_rm $@
-        elif [ "$CMD" = "restart" ]; then
-          docker_restart $@
-        elif [ "$CMD" = "start" ]; then
-          docker_start $@
-        elif [ "$CMD" = "exec" ]; then
-          docker_exec $@
-        elif [ "$CMD" = "stop" ]; then
-          docker_stop $@
-        elif [ "$CMD" = "logs" ]; then
-          docker_logs $@
-        elif [ "$CMD" = "ps" ]; then
-          docker_ps $@
-        else
-          docker $CMD $@
-        fi
-    fi
 }
