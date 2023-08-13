edbda6c7b36aca56c883eda4dec89e22492c557d
Kelvin Petry <test@test.de>
2023-08-13 20:27:34 +0200

    update

--- scripts/launch_polybar.sh
+++ scripts/launch_polybar.sh
@@ -1,6 +1,6 @@
 if type "xrandr"; then
   for m in $(polybar --list-monitors | cut -d":" -f1); do
-    MONITOR=$m polybar --reload example &
+    MONITOR=$m polybar --reload orchid1 &
   done
 else
   polybar --reload orchid &
