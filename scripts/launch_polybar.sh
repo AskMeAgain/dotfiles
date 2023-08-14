91ba1738126bf4079862bb3d0cc150e29d2f4eae
Kelvin Petry <test@test.de>
2023-08-14 20:24:04 +0200

    fixing polybar launcher

--- scripts/launch_polybar.sh
+++ scripts/launch_polybar.sh
@@ -1,6 +1,6 @@
 if type "xrandr"; then
   for m in $(polybar --list-monitors | cut -d":" -f1); do
-    MONITOR=$m polybar --reload orchid1 &
+    MONITOR=$m polybar --reload orchid &
   done
 else
   polybar --reload orchid &
