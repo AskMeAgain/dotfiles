aea805465442584ca863b7a6def6bbddddb2def2
Kelvin Petry <20632124+AskMeAgain@users.noreply.github.com>
2024-07-01 18:19:38 +0200

    Delete scripts/alias.sh

--- scripts/alias.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-alias ccc="xclip -sel clip"
-alias ls='ls --color=auto'
-alias grep='grep --color=auto'
-alias shutdown='shutdown now'
-alias rma='docker rm -f $(docker ps -a -q)'
-alias upgrade-system='echo '1234' | sudo -S pacman-mirrors --fasttrack 5 && echo '1234' | sudo -S pacman -Syyu --noconfirm'
